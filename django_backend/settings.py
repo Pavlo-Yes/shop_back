0a47bd779c3c3f07b35e0ff3a6c6524b65860708
Pavlo-Yes <yespavlo@gmail.com>
2021-04-19 16:21:14 +0300

    remove cors-headers from settings

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     # my apps
     'user',
     'products',
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
